dc2444c7be5230b4b77cbf4e67e9cee2bfb763c8
samiaahmed21 <sumyyah21@gmail.com>
2025-09-11 05:32:54 -0400

    Add AppointmentFormIC and DoctorCardIC updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import Landing_page from './Components/Landing_Page/Landing_page';
+import Landing_Page from './Components/Landing_Page/Landing_page';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
-import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation'
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Navbar /> 
 
       <Routes>
-        <Route path="/" element={<Landing_page />} />
+        <Route path="/" element={<Landing_Page />} />
         <Route path="/signup" element={<Sign_Up />} />
         <Route path="/login" element={<Login />} />
          <Route path="/instant-consultation" element={<InstantConsultation />} />
